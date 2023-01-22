217abc81c7ad1c7222ae0399fce3890b4215b13a
mgtezak <mgtezak@gmail.com>
2023-01-22 20:21:41 +0100

    add 5, modify 2

--- 2019/Day_02.py
+++ 2019/Day_02.py
@@ -1,26 +1,26 @@
 path = 'Advent_of_Code/2019/puzzle_input/02.txt'
 
 with open(path) as input:
-    data = input.read().split(',')
+    instructions = list(map(int, input.read().split(',')))
 
-def calc(noun, verb, part2=False):
-    l = list(map(int, data))
-    l[1], l[2] = noun, verb
-    for i, val in enumerate(l):
+def run_intcode(noun=12, verb=2):
+    nums = instructions.copy()
+    nums[1], nums[2] = noun, verb
+    for i, val in enumerate(nums):
         if i % 4 == 0:  
             if val == 1:
-                l[l[i+3]] = l[l[i+1]] + l[l[i+2]]
+                nums[nums[i+3]] = nums[nums[i+1]] + nums[nums[i+2]]
             elif val == 2:
-                l[l[i+3]] = l[l[i+1]] * l[l[i+2]]
+                nums[nums[i+3]] = nums[nums[i+1]] * nums[nums[i+2]]
             elif val == 99:
                 break
-    return l[0] == 19690720 if part2 else l[0] 
+    return nums[0]
         
 def get_noun_verb_comb():
     for noun in range(100):
         for verb in range(100):
-            if calc(noun, verb, True):
+            if run_intcode(noun, verb) == 19690720:
                 return 100 * noun + verb
 
-print(f'Part 1: {calc(12, 2)}')
+print(f'Part 1: {run_intcode()}')
 print(f'Part 2: {get_noun_verb_comb()}')

f11bf8473ab4de40949da16b44ce4360115ae0ae
mgtezak <mgtezak@gmail.com>
2023-12-08 14:09:25 +0100

    update

---
+++ 2023/Day_08.py
@@ -0,0 +1,47 @@
+from itertools import cycle
+import re
+import math
+
+with open('Advent_of_Code/2023/puzzle_input/08.txt', 'r') as f:
+    puzzle_input = f.read()
+
+
+def part1(puzzle_input):
+    directions, connections = puzzle_input.split('\n\n')
+    directions = cycle(0 if d == 'L' else 1 for d in directions)
+    graph = {}
+    regex = r'(\w{3}) = \((\w{3}), (\w{3})\)'
+    for node, left, right in re.findall(regex, connections):
+        graph[node] = [left, right]
+
+    node = 'AAA'
+    for steps, d in enumerate(directions, start=1):
+        node = graph[node][d]
+        if node == 'ZZZ':
+            break
+
+    return steps
+
+
+def part2(puzzle_input):
+    directions, connections = puzzle_input.split('\n\n')
+    directions = [0 if d == 'L' else 1 for d in directions]
+    graph = {}
+    regex = r'(\w{3}) = \((\w{3}), (\w{3})\)'
+    for node, left, right in re.findall(regex, connections):
+        graph[node] = [left, right]
+
+    starting_nodes = [node for node in graph if node[2] == 'A']
+    cycles = []
+    for node in starting_nodes:
+        for steps, d in enumerate(cycle(directions), start=1):
+            node = graph[node][d]
+            if node[2] == 'Z':
+                cycles.append(steps)
+                break
+
+    return math.lcm(*cycles)
+
+
+print('Part 1:', part1(puzzle_input))
+print('Part 2:', part2(puzzle_input))

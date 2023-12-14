1cf67d6eea01b2a41612a8a9e15a0681cb504211
mgtezak <mgtezak@gmail.com>
2023-12-14 16:02:12 +0100

    add day 12

---
+++ 2023/Day_12.py
@@ -0,0 +1,69 @@
+from functools import cache
+
+with open('Advent_of_Code/2023/puzzle_input/12.txt', 'r') as f:
+    puzzle_input = f.read()
+
+
+def part1(puzzle_input):
+
+    @cache
+    def dfs(sequence, groups):
+        if not groups:
+            return '#' not in sequence
+        seq_len = len(sequence)
+        group_len = groups[0]
+        if seq_len - sum(groups) - len(groups) + 1 < 0:
+            return 0
+        has_holes = any(sequence[x] == '.' for x in range(group_len))
+        if seq_len == group_len:
+            return 0 if has_holes else 1
+        can_use = not has_holes and (sequence[group_len] != '#')
+        if sequence[0] == '#':
+            return dfs(sequence[group_len+1:].lstrip('.'), tuple(groups[1:])) if can_use else 0
+        skip = dfs(sequence[1:].lstrip('.'), groups)
+        if not can_use:
+            return skip
+        return skip + dfs(sequence[group_len+1:].lstrip('.'), tuple(groups[1:]))
+                        
+    total = 0
+    for line in puzzle_input.split('\n'):
+        sequence, groups = line.split()
+        groups = [int(g) for g in groups.split(',')]
+        total += dfs(sequence, tuple(groups))
+        
+    return total
+
+
+def part2(puzzle_input):
+
+    @cache
+    def dfs(sequence, groups):
+        if not groups:
+            return '#' not in sequence
+        seq_len = len(sequence)
+        group_len = groups[0]
+        if seq_len - sum(groups) - len(groups) + 1 < 0:
+            return 0
+        has_holes = any(sequence[x] == '.' for x in range(group_len))
+        if seq_len == group_len:
+            return 0 if has_holes else 1
+        can_use = not has_holes and (sequence[group_len] != '#')
+        if sequence[0] == '#':
+            return dfs(sequence[group_len+1:].lstrip('.'), tuple(groups[1:])) if can_use else 0
+        skip = dfs(sequence[1:].lstrip('.'), groups)
+        if not can_use:
+            return skip
+        return skip + dfs(sequence[group_len+1:].lstrip('.'), tuple(groups[1:]))
+        
+    total = 0
+    for line in puzzle_input.split('\n'):
+        sequence, groups = line.split()
+        sequence = '?'.join([sequence] * 5).lstrip('.')
+        groups = [int(g) for g in groups.split(',')] * 5
+        total += dfs(sequence, tuple(groups))
+    
+    return total
+
+
+print('Part 1:', part1(puzzle_input))
+print('Part 2:', part2(puzzle_input))

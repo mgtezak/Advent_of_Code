5885c869be79ceb2a4b412da1de2279e7796c6dc
mgtezak <mgtezak@gmail.com>
2024-01-11 17:02:48 +0100

    update

--- 2023/Day_04.py
+++ 2023/Day_04.py
@@ -5,32 +5,29 @@ with open('Advent_of_Code/2023/puzzle_input/04.txt', 'r') as f:
 
 
 def part1(puzzle_input):
-    regex = r':(.*?)\|(.*)'
+    regex = r':(.*)\|(.*)'
     points = 0
-    for line in puzzle_input.split('\n'):
-        nums = re.search(regex, line)
-        win_nums = set(map(int, nums.group(1).split()))
-        true_nums = set(map(int, nums.group(2).split()))
-        n_matches = len(win_nums & true_nums)
-        if n_matches:
-            points += 2 ** (n_matches - 1)
+    for win_nums, true_nums in re.findall(regex, puzzle_input):
+        overlap = set(win_nums.split()) & set(true_nums.split())
+        if overlap:
+            points += 2 ** (len(overlap) - 1)
 
     return points
 
 
+
 def part2(puzzle_input):
     lines = puzzle_input.split('\n')
+    regex = r':(.*)\|(.*)'
     cards = [1] * len(lines)
-    regex = r':(.*?)\|(.*)'
     for i, line in enumerate(lines):
-        nums = re.search(regex, line)
-        win_nums = set(map(int, nums.group(1).split()))
-        true_nums = set(map(int, nums.group(2).split()))
-        n_matches = len(win_nums & true_nums)
-        for j in range(i + 1, i + 1 + n_matches):
-            cards[j] += cards[i]
+        win_nums, actual_nums = re.findall(regex, line)[0]
+        overlap = set(win_nums.split()) & set(actual_nums.split())
+        for j in range(len(overlap)):
+            cards[i+j+1] += cards[i]
     
     return sum(cards)
+
 
 
 print('Part 1:', part1(puzzle_input))

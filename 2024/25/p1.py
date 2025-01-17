23cf971786c8b75801e83366b4b80ebfb3e88932
mgtezak <michaeltezak@gedankenspieler.org>
2025-01-17 03:15:59 +0100

    day 25

--- 2024/25/p1.py
+++ 2024/25/p1.py
@@ -1,31 +1,16 @@
+from collections import defaultdict
+
+
 def part1(puzzle_input):
-
-    def convert(segment):
-        char = segment[0]
-        grid = segment.split('\n')
-        heights = [0] * 5
-        for col in range(5):
-            for row in reversed(range(6)):
-                if grid[row][col] == char:
-                    heights[col] = row
-                    break
-
-        return heights
+    locks_and_keys = defaultdict(list)
+    for seg in puzzle_input.split('\n\n'):
+        locks_and_keys[seg[0]].append([
+            col.count('#') for col in zip(*seg.split('\n'))
+        ])
     
-    locks, keys = [], []
-    for segment in puzzle_input.split('\n\n'):
-        if segment.startswith('#'):
-            locks.append(convert(segment))
-        else:
-            keys.append(convert(segment))
-
-    total = 0
-    for lock in locks:
-        for key in keys:
-            for i, j in zip(lock, key):
-                if i > j:
-                    break
-            else:
-                total += 1
-
-    return total
+    fit = 0
+    for lock in locks_and_keys['.']:
+        for key in locks_and_keys['#']:
+            fit += all(i + j < 8 for i, j in zip(lock, key))
+            
+    return fit

f2b0b7b93061af42c064665d96427e3dba6f10d8
privat <privat@mac.home>
2025-12-04 00:38:53 +0000

    add 2025/02

---
+++ 2025/02/p1.py
@@ -0,0 +1,10 @@
+def part1(puzzle_input):
+    ranges = [list(map(int, pair.split("-"))) for pair in puzzle_input.split(",")]
+    invalid = 0 
+    for i in range(1, 100_000):
+        candidate = int(str(i) * 2)
+        for lower, upper in ranges:
+            if lower <= candidate <= upper:
+                invalid += candidate
+
+    return invalid
